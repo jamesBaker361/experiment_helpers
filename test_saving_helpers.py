5a417bddcff7d6e19ed1feb3561a3263e539d250
James Baker <jamesbaker@Jamess-MacBook-Air-104.local>
2025-12-01 21:28:42 -0500

    load tests

--- test_saving_helpers.py
+++ test_saving_helpers.py
@@ -9,11 +9,32 @@ from accelerate import Accelerator
 from utils_test import *
 from saving_helpers import save_and_load_functions
 from huggingface_hub import HfApi
+import string
+import random
 
 class SavingTest(unittest.TestCase):
     def setUp(self):
         self.accelerator=Accelerator(gradient_accumulation_steps=4)
         
+    def test_load(self):
+        for flag in [True, False]:
+            with self.subTest(flag=flag):
+                folder=''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+                repo_id=''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+                # Source - https://stackoverflow.com/a/2257449
+                # Posted by Ignacio Vazquez-Abrams, modified by community. See post 'Timeline' for change history
+                # Retrieved 2025-12-01, License - CC BY-SA 4.0
+
+                
+
+                mnist_model=get_mnist_model()
+                api=HfApi()
+                save,load=save_and_load_functions({"mnist_pytorch_model.safetensors":mnist_model},
+                                                folder,
+                                                api,
+                                                f"jlbaker361/{repo_id}")
+                self.assertEqual(load(flag),load(flag))
+    
     def test_save_and_load(self):
         for flag in [True, False]:
             with self.subTest(flag=flag):
