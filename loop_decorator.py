fa159fa042d943b75b05eaaac9c2961cfced8546
James Baker <jamesbaker@Jamess-MacBook-Air-104.local>
2025-12-03 23:07:22 -0500

    many changes

--- loop_decorator.py
+++ loop_decorator.py
@@ -73,7 +73,7 @@ def optimization_loop(accelerator:Accelerator,
                             break
                         loss=function(batch,False,{
                         "b":b,
-                        "epochs":e,
+                        "e":-1,
                         "mode":"test"})
                         test_loss_buffer.append(loss)
                     end=time.time()
