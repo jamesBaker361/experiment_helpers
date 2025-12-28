86c844aa34ced395cd29b38bc4b96af41ae76157
James Baker <jamesbaker@Jamess-MacBook-Air-331.local>
2025-12-28 12:37:21 -0500

    hf delete

---
+++ hf_helpers.py
@@ -0,0 +1,37 @@
+from huggingface_hub import HfApi
+import argparse
+
+def delete_models(api:HfApi):
+    models = api.list_models(author=api.whoami()["name"])
+    for repo in models:
+        print("Deleting model:", repo.modelId)
+        api.delete_repo(repo_id=repo.modelId, repo_type="model")
+        
+def delete_datasets(api:HfApi):
+    datasets = api.list_datasets(author=api.whoami()["name"])
+    for repo in datasets:
+        print("Deleting dataset:", repo.id)
+        api.delete_repo(repo_id=repo.id, repo_type="dataset")
+
+if __name__=="__main__":
+    api = HfApi()
+
+    # list all repos you own
+    models = api.list_models(author=api.whoami()["name"])
+    datasets = api.list_datasets(author=api.whoami()["name"])
+    spaces = api.list_spaces(author=api.whoami()["name"])
+    print(f"Models: {len(models)}")
+    print(f"Datasets: {len(datasets)}")
+    print(f"Spaces: {len(spaces)}")
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--delete_models",action="store_true")
+    parser.add_argument("--delete_datasets",action="store_true")
+    args=parser.parse_args()
+    print(args)
+    if args.delete_models:
+        delete_models(api)
+            
+    if args.delete_datasets:
+        delete_datasets(api)
+            
+        
