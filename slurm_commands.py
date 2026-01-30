0adcf4a94e735759aa756757678fdf07eb5355b9
James Baker <jlbaker361@gmail.com>
2026-01-30 16:07:49 -0500

    parititon

--- slurm_commands.py
+++ slurm_commands.py
@@ -4,6 +4,6 @@ for file in os.listdir(os.getcwd()):
     if file.endswith(".py"):
         print(file)
         for partition in ["cpu","gpu"]:
-            print(f"sbatch -J test --err=slurm_chip/test/{file}_{partition}.err --out=slurm_chip/test/{file}_{partition}.out runpycpu_chip.sh {file}")
-            print(f"sbatch -J test --err=slurm_chip/test/{file}_{partition}.err --out=slurm_chip/test/{file}_{partition}.out runpygpu_chip.sh {file}")
+            print(f"sbatch -J test --err=slurm_chip/test/{file}_{partition}.err --out=slurm_chip/test/{file}_{partition}.out runpy{partition}_chip.sh {file}")
+            print(f"sbatch -J test --err=slurm_chip/test/{file}_{partition}.err --out=slurm_chip/test/{file}_{partition}.out runpy{partition}_chip.sh {file}")
         print("")
