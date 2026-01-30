96bd8736104636d7df1ab08338721afbc14aa3cc
James Baker <jlbaker361@gmail.com>
2026-01-30 13:36:13 -0500

    newline

--- slurm_commands.py
+++ slurm_commands.py
@@ -2,5 +2,7 @@ import os
 
 for file in os.listdir(os.getcwd()):
     if file.endswith(".py"):
+        print(file)
         print(f"sbatch -J test --err=slurm_chip/test/{file}.err --out=slurm_chip/test/{file}.out runpycpu_chip.sh {file}")
         print(f"sbatch -J test --err=slurm_chip/test/{file}.err --out=slurm_chip/test/{file}.out runpygpu_chip.sh {file}")
+        print("")
